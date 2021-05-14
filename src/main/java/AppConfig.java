aa614e487fc8e7b04d9e17617311344f4bffd413
boris.milicevic <boris31094@gmail.com>
2021-05-14 20:17:18 +0200

    Spring Framework: Spring Fundamentals
    - Spring Scopes

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,16 +2,36 @@ import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
+    /**
+     * Named bean speakerService
+     * Two demonstrates two ways of dependency injection:
+     * setter and constructor
+     * @Scope annotation:
+     * - singleton (default) One instance per Spring Container (Application Context)
+     * - prototype Unique instance per request from the bean container
+     * Web-aware spring project can recognise additional scopes:
+     * - request Bean per http request - while user request is served
+     * - session Bean per http session - as long as user session is alive
+     * - global Single bean per application - as long as application runs on the server
+     */
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         return getSpeakerServiceViaConstructorInjection();
 //        return getSpeakerServiceViaSetterInjection();
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
     }
 
     private SpeakerService getSpeakerServiceViaSetterInjection() {
@@ -22,10 +42,5 @@ public class AppConfig {
 
     private SpeakerService getSpeakerServiceViaConstructorInjection() {
         return new SpeakerServiceImpl(getSpeakerRepository());
-    }
-
-    @Bean(name = "speakerRepository")
-    public SpeakerRepository getSpeakerRepository() {
-        return new HibernateSpeakerRepositoryImpl();
     }
 }
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,6 +7,8 @@ public class Application {
     public static void main(String args[]) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
-        System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service);
+        SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
     }
 }
