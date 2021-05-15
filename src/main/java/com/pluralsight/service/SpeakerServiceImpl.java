6af70f0071d7500d1ccf4677b5f73636a2f513e9
boris.milicevic <boris31094@gmail.com>
2021-05-15 09:34:29 +0200

    Spring Framework: Spring Fundamentals
    - Spring XML Configuration setter injection

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,13 +1,16 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepository speakerRepository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository speakerRepository;
+
+    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
+    }
 
     @Override
     public List<Speaker> findAll() {
